38a3e9c5a92e491ab4cd778e061aa73f343a1d9f
Leonardo Tumadjian <tumadjian@gmail.com>
2017-09-11 15:05:03 -0300

    Initial commit

---
+++ src/Controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+// Composer autoload
+// PSR-4 - PHPFIG
+// vendor name
+// Vendor\Pasta\Subpasta\NomeClasse
+namespace MeuProjeto\Controllers;
+
+class Home
+{
+    public function index()
+    {
+        echo 'Hello mundo';
+    }
+}
---
+++ src/Models/Cliente.php
@@ -0,0 +1,10 @@
+<?php
+namespace MeuProjeto\Models;
+
+class Cliente
+{
+    public function getAll()
+    {
+        echo 'Retorna todos os clientes';
+    }
+}
